df75ddc634152c386fa2a9335f1ca0637cae52da
michael <michael.james.ashworth@gmail.com>
2021-12-16 02:53:13 +0000

    Add conditional to form submit function that checks if user has placed too many characters + if the character field is empty and returns an alert

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,45 +1,54 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+// const data = [
+//   {
+//     user: {
+//       name: "Newton",
+//       avatars: "https://i.imgur.com/73hZDYK.png",
+//       handle: "@SirIsaac",
+//     },
+//     content: {
+//       text: "If I have seen further it is by standing on the shoulders of giants",
+//     },
+//     created_at: 1461116232227,
+//   },
+//   {
+//     user: {
+//       name: "Descartes",
+//       avatars: "https://i.imgur.com/nlhLi3I.png",
+//       handle: "@rd",
+//     },
+//     content: {
+//       text: "Je pense , donc je suis",
+//     },
+//     created_at: 1461113959088,
+//   },
+// ];
 
+//SHORTHAND VERSION OF .AJAX
 const loadTweets = function() {
-
+  $.get("/tweets/", function($tweet) {
+    renderTweets($tweet);
+  });
 };
-  //   $.get("/tweets/", serializedData, function() {
 
-// };
+
+//REGULAR .AJAX WAY
+// $.ajax({
+//   url: "/tweets/",
+//   method: "GET",
+// }).done(function () {
+//   renderTweets($tweet);
+// });
 
 //HELPER FUNCTION THAT CALLS createTweetElement() AND RENDERS IT TO THE WEBPAGE
-const renderTweets = function(tweets) {
+const renderTweets = function (tweets) {
   for (const tweet of tweets) {
     let $tweet = createTweetElement(tweet);
-    $('#tweets-container').prepend($tweet);
+    $("#tweets-container").prepend($tweet);
   }
 };
 
 //HELPER FUNCTION THAT MAKES THE TWEETS DYNAMIC BY USING OUR HTML SKELETON AND RETURNING IT AS A VARIABLE
-const createTweetElement = function(data) {
+const createTweetElement = function (data) {
   const $tweet = $(`<article class="tweet">
   <header>
     <div class="userGroup">
@@ -61,22 +70,28 @@ const createTweetElement = function(data) {
   return $tweet;
 };
 
-$(document).ready(function() {
-  renderTweets(data);
+$(document).ready(function () {
+  loadTweets();
 });
 
-$(document).ready(function() {
-
-});
-
-
-$(document).ready(function() {
-  $('form').submit(function(event) {
+$(document).ready(function () {
+  $("form").submit(function (event) {
     event.preventDefault();
     let serializedData = $(this).serialize();
-    $.post("/tweets/", serializedData, function() {
-      console.log("sucessfully posted:", serializedData);
-    });
+    //verify here before posting
+    if ($('textarea#tweet-text').val().length > 140) {
+      alert("It looks like you've entered too many characters");
+    }
+    if ($('textarea#tweet-text').val().length === 0) {
+      alert("It looks like you're tweet is empty!");
+    }
+    if ($('textarea#tweet-text').val() === null) {
+      alert("It looks like you didn't tweet anything");
+    }
+    else {
+      $.post("/tweets/", serializedData, function () {
+        console.log("sucessfully posted:", serializedData);
+      });
+    }
   });
 });
-
