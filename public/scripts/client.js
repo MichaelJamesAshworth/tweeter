9c10c3d37c1a0737b5c2ec6979b176476b5fbaa3
michael <michael.james.ashworth@gmail.com>
2021-12-16 04:20:18 +0000

    Add functionality to main page. Can now loadTweets without having to refresh the page. Additionally, protect page from XXS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,46 +1,17 @@
-// const data = [
-//   {
-//     user: {
-//       name: "Newton",
-//       avatars: "https://i.imgur.com/73hZDYK.png",
-//       handle: "@SirIsaac",
-//     },
-//     content: {
-//       text: "If I have seen further it is by standing on the shoulders of giants",
-//     },
-//     created_at: 1461116232227,
-//   },
-//   {
-//     user: {
-//       name: "Descartes",
-//       avatars: "https://i.imgur.com/nlhLi3I.png",
-//       handle: "@rd",
-//     },
-//     content: {
-//       text: "Je pense , donc je suis",
-//     },
-//     created_at: 1461113959088,
-//   },
-// ];
-
+const escapeText = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 //SHORTHAND VERSION OF .AJAX
 const loadTweets = function() {
-  $.get("/tweets/", function($tweet) {
-    renderTweets($tweet);
+  $.get("/tweets/", function($tweets) {
+    renderTweets($tweets);
   });
 };
 
-
-//REGULAR .AJAX WAY
-// $.ajax({
-//   url: "/tweets/",
-//   method: "GET",
-// }).done(function () {
-//   renderTweets($tweet);
-// });
-
 //HELPER FUNCTION THAT CALLS createTweetElement() AND RENDERS IT TO THE WEBPAGE
-const renderTweets = function (tweets) {
+const renderTweets = function(tweets) {
   for (const tweet of tweets) {
     let $tweet = createTweetElement(tweet);
     $("#tweets-container").prepend($tweet);
@@ -48,7 +19,7 @@ const renderTweets = function (tweets) {
 };
 
 //HELPER FUNCTION THAT MAKES THE TWEETS DYNAMIC BY USING OUR HTML SKELETON AND RETURNING IT AS A VARIABLE
-const createTweetElement = function (data) {
+const createTweetElement = function(data) {
   const $tweet = $(`<article class="tweet">
   <header>
     <div class="userGroup">
@@ -57,7 +28,7 @@ const createTweetElement = function (data) {
     </div>
     <span>${data.user.handle}</span>
   </header>
-  <p>${data.content.text}</p> 
+  <p>${escapeText(data.content.text)}</p> 
   <footer>
     <time>${timeago.format(data.created_at)}</time>
     <div class="iconGroup">
@@ -70,27 +41,25 @@ const createTweetElement = function (data) {
   return $tweet;
 };
 
-$(document).ready(function () {
+$(document).ready(function() {
   loadTweets();
 });
 
-$(document).ready(function () {
-  $("form").submit(function (event) {
+$(document).ready(function() {
+  $("form").submit(function(event) {
     event.preventDefault();
     let serializedData = $(this).serialize();
-    //verify here before posting
-    if ($('textarea#tweet-text').val().length > 140) {
-      alert("It looks like you've entered too many characters");
+    if ($("textarea#tweet-text").val().length > 140) {
+      alert("Ooops! It looks like you've entered too many characters.");
     }
-    if ($('textarea#tweet-text').val().length === 0) {
+    if ($("textarea#tweet-text").val().length === 0) {
       alert("It looks like you're tweet is empty!");
     }
-    if ($('textarea#tweet-text').val() === null) {
-      alert("It looks like you didn't tweet anything");
-    }
-    else {
-      $.post("/tweets/", serializedData, function () {
-        console.log("sucessfully posted:", serializedData);
+    if ($("textarea#tweet-text").val() === null) {
+      alert("It looks like your tweet is empty!");
+    } else {
+      $.post("/tweets/", serializedData, function() {
+        loadTweets();
       });
     }
   });
